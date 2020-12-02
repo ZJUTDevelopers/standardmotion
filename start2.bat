b33d2e251ef73aa969f72088a133c244cd65841d
gorgeous <gorgeousdays@outlook.com>
2020-12-02 17:28:41 +0800

    实现上传与摄像头检测的json保存以及ststartrtrt—_openpose操作

---
+++ start2.bat
@@ -0,0 +1,3 @@
+title running_openpose
+cd .\openpose
+.\bin\OpenPoseDemo.exe --video C:/Users/Hp/Desktop/VID_20201121_190625.mp4 --net_resolution 320x176 --number_people_max 1 --tracking 7 --write_json ..\json2 --keypoint_scale 3 --frame_flip true
